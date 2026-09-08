46524d35de1b43b5f0826ae0373431f3c5129f05
Mac <mac@MacBook-Pro-Mac.local>
2026-09-08 22:29:09 +0300

    Add 26th model: Adidas Handball Spezial Grey Black 3763 (2140 UAH)

--- script.js
+++ script.js
@@ -110,6 +110,8 @@ function updateFormPrice() {
         priceDisplay.textContent = '2 350 грн';
     } else if (val.includes('2200')) {
         priceDisplay.textContent = '2 200 грн';
+    } else if (val.includes('2140')) {
+        priceDisplay.textContent = '2 140 грн';
     } else if (val.includes('1690')) {
         priceDisplay.textContent = '1 690 грн';
     } else {
